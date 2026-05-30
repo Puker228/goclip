0bbc655dea1fdd5b31fc03fe054fee4a12a43cfc
puker228 <pochta@pochta.com>
2026-05-31 02:40:03 +0300

    add clipboard watch

--- cmd/goclip/main.go
+++ cmd/goclip/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"golang.design/x/clipboard"
 )
 
 func main() {
-	err := clipboard.Init()
-	if err != nil {
+	if err := clipboard.Init(); err != nil {
 		panic(err)
 	}
 
-	data := clipboard.Read(clipboard.FmtText)
+	var history []string
+	seen := map[string]bool{}
 
-	if len(data) == 0 {
-		fmt.Println("clipboard is empty")
-	} else {
-		fmt.Printf("last clipboard element: %s\n", string(data))
+	add := func(data []byte) {
+		if len(data) == 0 {
+			return
+		}
+
+		text := string(data)
+		if seen[text] {
+			return
+		}
+
+		seen[text] = true
+		history = append(history, text)
+
+		fmt.Println("clipboard history:")
+		for i, item := range history {
+			fmt.Printf("%d: %s\n", i+1, item)
+		}
+		fmt.Println()
+	}
+
+	add(clipboard.Read(clipboard.FmtText))
+
+	for data := range clipboard.Watch(context.Background(), clipboard.FmtText) {
+		add(data)
 	}
 }
